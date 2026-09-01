9bf5135fd51b96b355e9b0f9a5da23772d564d38
Santhosh R <santhosh.rv.work@gmail.com>
2026-09-01 13:36:02 +0530

    Time: 27 ms (90.61%), Space: 20.2 MB (73.1%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -0,0 +1,19 @@
+class Solution:
+    def productExceptSelf(self,nums):
+
+        n = len(nums)
+        answer = [1] * n
+
+        prefix = 1
+
+        for i in range(n):
+            answer[i] = prefix
+            prefix = prefix * nums[i]
+
+        suffix = 1
+
+        for i in range(n - 1, -1 , -1):
+            answer[i] = answer[i] * suffix
+            suffix = suffix * nums[i]
+
+        return answer
